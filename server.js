494fac5ba9f85bf4d801c549d23063ff27d7d938
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-03-06 18:34:53 -0700

    Start and test routes

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
-    res.send('Testing 123');
+    // TODO: Send all jobs
+    res.send('Get');
 });
+
+app.post('/', (req, res) => {
+    // TODO: Add job to the list of jobs
+    res.send('Post');
+});
+
+app.put('/', (req, res) => {
+    // TODO: Update job
+    res.send('Put');
+});
+
+app.delete('/', (req, res) => {
+    // TODO: Delete job
+    res.send('Delete');
+});
+
+
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`);
