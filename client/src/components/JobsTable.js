cec5cfd587e98601b9272e58422ca8a92e0da276
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-03-10 21:06:54 -0600

    Add delete button and functionality to front-end

--- client/src/components/JobsTable.js
+++ client/src/components/JobsTable.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
-import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function JobsTable() {
     const [jobs, setJobs] = useState([]);
+
+
+    const deleteJob = (id) => {
+        try {
+            fetch(`http://localhost:8000/jobs?id=${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json());
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
     useEffect(() => {
         fetch('http://localhost:8000/jobs')
             .then(res => res.json())
             .then(data => setJobs(data));
     }, []);
+
+
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -22,6 +41,11 @@ export default function JobsTable() {
                         <TableRow key={job._id}>
                             <TableCell>{job.title}</TableCell>
                             <TableCell>{job.hours}</TableCell>
+                            <TableCell>
+                                <IconButton onClick={() => { deleteJob(job._id) }}>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
