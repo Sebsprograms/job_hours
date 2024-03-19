b49f16e8568067e8da4190f8588d695fe4df9224
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-03-18 19:19:30 -0600

    Add routes for setting hours, deleting hours and updating hours

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -4,3 +4,7 @@ export const isValidDate = (date) => {
     return !isNaN(new Date(date));
 }
 
+
+export const sanitizeDate = (date) => {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+}
